9dbdf7cdc2e7f0d63a167a8bab58abafac50eb3b
taugat <taugat@github.com>
2023-09-22 13:57:24 +0100

    adding details on factory class

--- src/main/java/com/etsugo/designpattern/abstractfactory/ElectronicFactory.java
+++ src/main/java/com/etsugo/designpattern/abstractfactory/ElectronicFactory.java
@@ -5,9 +5,10 @@ import com.etsugo.designpattern.factorymethod.AircraftCreator;
 
 /**
  * Abstract Factory class :
- * Electronic factory can create different kind of electronic components: {@link Phone} and {@link Headphone}.
+ * A factory class let us work with various families of related products.
+ * Here, electronic factory can create different kind of electronic components: {@link Phone} and {@link Headphone}.
  * As for {@link AircraftCreator#createAircraft() factory method}, a factory method can be extended by any kind of factory.
- * Extend this class to implement Factory Methods (as {@link AppleFactory}).
+ * Extend this class to implement Factory Methods in a concrete factory class as {@link AppleFactory}.
  */
 public abstract class ElectronicFactory
 {
